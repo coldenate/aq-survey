1b54af579c42019b8349524b833381da748fe797
Nathan Solis <66754842+coldenate@users.noreply.github.com>
2023-01-01 18:47:08 -0600

    inject survey

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useState, useRef } from "react";
+import "./App.css";
+
+import "survey-core/defaultV2.min.css";
+import { StylesManager, Model } from "survey-core";
+import { Survey } from "survey-react-ui";
+import surveyJson from "./survey.json";
+import "./index.css";
+
+// StylesManager.applyTheme("defaultV2");
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// useRef enables the Model object to persist between state changes
+	const survey = useRef(new Model(surveyJson)).current;
+	const [surveyResults, setSurveyResults] = useState("");
+	const [isSurveyCompleted, setIsSurveyCompleted] = useState(false);
+
+	const displayResults = useCallback((sender) => {
+		setSurveyResults(JSON.stringify(sender.data, null, 4));
+		setIsSurveyCompleted(true);
+	}, []);
+
+	survey.onComplete.add(displayResults);
+
+	return (
+		<>
+			<Survey model={survey} id="surveyContainer" />
+			{isSurveyCompleted && (
+				<>
+					<p>Result JSON:</p>
+					<code style={{ whiteSpace: "pre" }}>{surveyResults}</code>
+				</>
+			)}
+		</>
+	);
 }
 
 export default App;
